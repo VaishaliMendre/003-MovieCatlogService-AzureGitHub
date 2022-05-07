8745f389b20dc05b4cc1b43060ee656e3284a384
Srivastava <vaishali.srivastava@capgemini.com>
2022-05-07 11:41:49 +0530

    target commit

---
+++ target/classes/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO mov_cat(mov_id,mov_name) VALUES(1001,'Hero');
+INSERT INTO mov_cat(mov_id,mov_name) VALUES(1002,'Dangle');
+INSERT INTO mov_cat(mov_id,mov_name) VALUES(1003,'American Beuty');
+INSERT INTO mov_cat(mov_id,mov_name) VALUES(1004,'Panipat');
+INSERT INTO mov_cat(mov_id,mov_name) VALUES(1005,'Laila');
+INSERT INTO mov_cat(mov_id,mov_name) VALUES(1006,'Mission Impossible');
